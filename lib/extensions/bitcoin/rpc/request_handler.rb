1ca76e559da795f23e0b82754ba4dc74978aa0e8
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2018-12-04 19:34:46 +0900

    Add new rpc 'createaccount' and override 'getnewaddress'

--- lib/extensions/bitcoin/rpc/request_handler.rb
+++ lib/extensions/bitcoin/rpc/request_handler.rb
@@ -18,6 +18,22 @@ module Bitcoin
       def getbalance(account_name)
         node.wallet.get_balance(account_name)
       end
+
+      # create new bitcoin address for receiving payments.
+      def getnewaddress(account_name)
+        address = node.wallet.generate_new_address(account_name)
+        script = Bitcoin::Script.parse_from_addr(address)
+        pubkey_hash = script.witness_data[1].bth
+        node.filter_add(pubkey_hash)
+        address
+      end
+
+      def createaccount(account_name)
+        account = node.wallet.create_account(Bitcoin::Wallet::Account::PURPOSE_TYPE[:native_segwit], account_name)
+        account.to_h
+      rescue
+        {}
+      end
     end
   end
 end
